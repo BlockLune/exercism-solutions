206939ed459d9dd114bfc8c2f14b09d38185d936
BlockLune <39331194+BlockLune@users.noreply.github.com>
2025-04-20 16:38:17 +0800

    feat: hamming

---
+++ rust/hamming/src/lib.rs
@@ -0,0 +1,16 @@
+/// Return the Hamming distance between the strings,
+/// or None if the lengths are mismatched.
+pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
+    if s1.len() != s2.len() {
+        return None;
+    }
+
+    let mut count = 0;
+    for (c1, c2) in s1.chars().zip(s2.chars()) {
+        if c1 != c2 {
+            count += 1;
+        }
+    }
+
+    Some(count)
+}
